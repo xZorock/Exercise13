68faf9d15f23caee4b5686dfba1c3f5bd3816907
Jason Di Bartolo <jasondibartolo98@gmail.com>
2022-12-12 17:10:44 +0100

    Initial Commit

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        String myName = "Jason";
+        if(myName.length() % 2 == 0){
+            System.out.println("The numbers of letters is even");
+        }else{
+            System.out.println("The numbers of letters is odd");
+        }
+    }
+}
